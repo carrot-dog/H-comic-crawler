806027d86b9db13fa846c44cd0a4707122e75a6f
qjfoidnh <qjfoidnh@126.com>
2018-08-22 12:26:11 +0800

    a version with coroutine in downloading

--- main2.py
+++ main2.py
@@ -1,14 +1,14 @@
-﻿#!/usr/bin/env python3
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on Thu Aug 16 16:41:38 2018
 
 @author: liu
 
-Python有PIL锁，多线程无法充分利用多核性能，所以使用多进程
+Python有GIL锁，多线程无法充分利用多核性能，所以使用多进程
 """
 
-from spider_module import Pic_saver
+from comics.spider_module import Pic_saver
 import multiprocessing
 
 if __name__ =="__main__":
--- settings.py
+++ settings.py
@@ -17,3 +17,7 @@ PASSWD='3233836'
 DB='comic_links'
 
 abs_path = "/person_files/comic_downloads/"
+
+head = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'
+                     }
+proxies = { "http": "http://127.0.0.1:55402", "https": "127.0.0.1:55402"}
